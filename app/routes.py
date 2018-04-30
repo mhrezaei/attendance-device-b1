1f8efb0635e8c1f87f0053cc11ea47875ef35df6
Majid Alaeinia <alaeinia.majid@gmail.com>
2018-04-30 11:36:49 +0430

    Added user_define route

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,7 @@
 from config import app, publish, store, socket
 from flask import render_template, request, flash, redirect, url_for
+from models import *
+from forms import UserDefineForm
 
 store['clients'] = []
 
@@ -38,3 +40,36 @@ def index():
 def settings():
     return render_template('settings.html')
 
+
+@app.route('/user_define', methods=['POST', 'GET'])
+def user_define():
+    # f.readImage() #TODO: sensor should be off when on this page, unless it is needed
+
+    form = UserDefineForm()
+
+    if request.method == 'POST':  # TODO: form validation
+        # Form NOT validated
+        if form.validate() == 0:  # form.validate == False
+            return render_template('user_define.html', form=form)
+        # Form validated correctly
+        elif form.validate() != 0:  # form.validate != False
+            the_first_name = request.form['first_name']
+            the_last_name = request.form['last_name']
+            the_code_melli = request.form['code_melli']
+
+            # TODO: (SOLVED) code_melli existence query
+            code_melli_existence_clause = db.table('users').where('code_melli', the_code_melli).count()
+            # code_melli_existence_clause = 'CODE MELLI EXISTS NOT'
+            # code_melli_existence_clause = db.table('users').where_exists(db.table('users').select(db.raw(1)).where_raw('users.code_melli' == 23))
+
+            # This code melli already exists.
+            if code_melli_existence_clause != 0:
+                flash('This code melli already exists.')
+                return render_template('user_define.html', form=form)
+            # This is a new code melli.
+            elif code_melli_existence_clause == 0:
+                db.table('users').insert(first_name=the_first_name, last_name=the_last_name, code_melli=the_code_melli)
+                flash('The user has been successfully defined.')
+                return render_template('user_define.html', form=form)
+    elif request.method == 'GET':
+        return render_template('user_define.html', form=form)
