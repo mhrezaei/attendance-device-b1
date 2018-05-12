384d67f89aa977c5642479f7dcd4eb919e70bd67
Majid Alaeinia <alaeinia.majid@gmail.com>
2018-05-12 16:34:47 +0430

    Added enroll_rfid_timeout to global_variables.py

--- app/globla_variables.py
+++ app/globla_variables.py
@@ -1,4 +1,5 @@
-global settings_timeout, enroll_finger_timeout, working_hours
-settings_timeout = 15
-enroll_finger_timeout = 5
+global settings_timeout, enroll_finger_timeout, working_hours, enroll_rfid_timeout
+settings_timeout = 15 # seconds
+enroll_finger_timeout = 5 # seconds
+enroll_rfid_timeout = 5 # seconds
 working_hours = 39600 # 11 hours
