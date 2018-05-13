8864b52ee86b8ac962e0a9b3386d3d64d1aa7ce2
Majid Alaeinia <alaeinia.majid@gmail.com>
2018-05-13 13:01:06 +0430

    Temporal change - no_action_allowed is 10 secs now

--- app/globla_variables.py
+++ app/globla_variables.py
@@ -3,4 +3,4 @@ settings_timeout = 15 # seconds
 enroll_finger_timeout = 5 # seconds
 enroll_rfid_timeout = 5 # seconds
 working_hours = 39600 # 11 hours
-no_action_allowed = 600 # 10 minutes
+no_action_allowed = 10 # 10 seconds #TODO: Consider changing it to 10 min whenever works fine
