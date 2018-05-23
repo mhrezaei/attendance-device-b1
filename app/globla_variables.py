c8cfd1970f52c69a3afb5a4bd5608d84a29486ca
Majid Alaeinia <alaeinia.majid@gmail.com>
2018-05-23 12:25:44 +0430

    Changed attendance_not_allowed_timeout from 10 min to 2 min

--- app/globla_variables.py
+++ app/globla_variables.py
@@ -3,4 +3,4 @@ settings_timeout = 15 # seconds
 enroll_finger_timeout = 5 # seconds
 enroll_rfid_timeout = 5 # seconds
 working_hours = 39600 # 11 hours
-attendance_not_allowed_timeout = 600 # 10 minutes
+attendance_not_allowed_timeout = 120 # 2 minutes
