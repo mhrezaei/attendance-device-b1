1ba4ebb26d67bfe0cbdc4334d0ca92c53eac9bb2
Majid Alaeinia <alaeinia.majid@gmail.com>
2018-07-17 18:17:31 +0430

    Add 'request_to_refresh_for_crud_on_laravel_period' to 'global_variables'

--- app/globla_variables.py
+++ app/globla_variables.py
@@ -3,11 +3,15 @@ global enroll_finger_timeout
 global working_hours
 global enroll_rfid_timeout
 global attendance_not_allowed_timeout
+
 global handle_the_is_synced_field_period
+global request_to_refresh_for_crud_on_laravel_period
 
 settings_timeout = 15  # 15 seconds
 enroll_finger_timeout = 5  # 5 seconds
 enroll_rfid_timeout = 5  # 5 seconds
 working_hours = 39600  # 11 hours
 attendance_not_allowed_timeout = 120  # 2 minutes
+
 handle_the_is_synced_field_period = 60  # 1 minute
+request_to_refresh_for_crud_on_laravel_period = 300  # 5 minutes
