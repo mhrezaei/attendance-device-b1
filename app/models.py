a1616049525eddfbff95112e2bda07b0aff3044b
Majid Alaeinia <alaeinia.majid@gmail.com>
2018-09-15 15:40:34 +0430

    add code docs

--- app/models.py
+++ app/models.py
@@ -17,16 +17,25 @@ Model.set_connection_resolver(db)
 
 
 class User(Model):
+    """
+    User model, associated with the 'users' table.
+    """
     __table__ = 'users'
     __guarded__ = ['id']
 
 
 class Finger(Model):
+    """
+    Finger model, associated with the 'fingers' table.
+    """
     __table__ = 'fingers'
     __guarded__ = ['id']
 
 
 class UserLog(Model):
+    """
+    UserLog model, associated with the 'user_logs' table.
+    """
     __table__ = 'user_logs'
     __guarded__ = ['id']
     __fillable__ = ['is_synced']
